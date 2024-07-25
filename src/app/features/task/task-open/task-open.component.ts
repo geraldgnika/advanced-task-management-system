905a04388244540f44076213ba1cadd4746d5e59
Gerald Nika <geraldgnika@gmail.com>
2024-07-25 16:55:57 +0200

    Small refactorings in commenting functionality

--- src/app/features/task/task-open/task-open.component.ts
+++ src/app/features/task/task-open/task-open.component.ts
@@ -1,206 +1,225 @@
-import { ChangeDetectionStrategy, Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	OnInit,
+	ElementRef,
+	ViewChild,
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { Store } from '@ngrx/store';
 import * as TaskActions from '../../../shared/_store/task/task.actions';
 import { Observable } from 'rxjs';
 import { AppState } from '../../../shared/_store/_common/app.state';
-import { selectSelectedTask, selectTasksError, selectTasksLoading } from '../../../shared/_store/task/task.selectors';
+import {
+	selectSelectedTask,
+	selectTasksError,
+	selectTasksLoading,
+} from '../../../shared/_store/task/task.selectors';
 import { AuthenticationService } from '../../../core/_services/authentication.service';
 import { Task } from '../../../core/types/interfaces/task';
 import { TaskService } from '../../../core/_services/task.service';
 
 @Component({
-  selector: 'app-task-open',
-  templateUrl: './task-open.component.html',
-  styleUrls: ['./task-open.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-task-open',
+	templateUrl: './task-open.component.html',
+	styleUrls: ['./task-open.component.css'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskOpenComponent implements OnInit {
-  task$: Observable<Task | null> | undefined;
-  loading$: Observable<boolean> | undefined;
-  error$: Observable<any> | undefined;
-  task!: Task;
-  newComment: string = '';
-  usn = this.authenticationService.getCurrentUser().username;
-  showMentionBox: boolean = false;
-  filteredUsernames: string[] = [];
-  allUsernames: string[] = [];
-  @ViewChild('commentInput') commentInput!: ElementRef<HTMLInputElement>;
+	task$: Observable<Task | null> | undefined;
+	loading$: Observable<boolean> | undefined;
+	error$: Observable<any> | undefined;
+	task!: Task;
+	newComment: string = '';
+	usn = this.authenticationService.getCurrentUser().username;
+	showMentionBox: boolean = false;
+	filteredUsernames: string[] = [];
+	allUsernames: string[] = [];
+	@ViewChild('commentInput') commentInput!: ElementRef<HTMLInputElement>;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private _location: Location,
-    private store: Store<AppState>,
-    public authenticationService: AuthenticationService,
-    private taskService: TaskService
-  ) {}
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router,
+		private _location: Location,
+		private store: Store<AppState>,
+		public authenticationService: AuthenticationService,
+		private taskService: TaskService
+	) {}
 
-  ngOnInit(): void {
-    this.getTask();
+	ngOnInit(): void {
+		this.getTask();
 
-    this.authenticationService.getUsers().subscribe(users => {
-      this.allUsernames = users.map(user => user.username);
-    });
-  }
+		this.authenticationService.getUsers().subscribe((users) => {
+			this.allUsernames = users.map((user) => user.username);
+		});
+	}
 
-  getTask() {
-    const taskId = this.route.snapshot.params['id'];
-    this.store.dispatch(TaskActions.loadTask({ id: taskId }));
+	getTask() {
+		const taskId = this.route.snapshot.params['id'];
+		this.store.dispatch(TaskActions.loadTask({ id: taskId }));
 
-    this.task$ = this.store.select(selectSelectedTask);
-    this.loading$ = this.store.select(selectTasksLoading);
-    this.error$ = this.store.select(selectTasksError);
-  }
+		this.task$ = this.store.select(selectSelectedTask);
+		this.loading$ = this.store.select(selectTasksLoading);
+		this.error$ = this.store.select(selectTasksError);
+	}
 
-  deleteTask(): void {
-    this.task$!.subscribe(task => {
-      if (!task) {
-        console.error('Task not found');
-        return;
-      }
+	deleteTask(): void {
+		this.task$!.subscribe((task) => {
+			if (!task) {
+				console.error('Task not found');
+				return;
+			}
 
-      this.task = task;
-    });
+			this.task = task;
+		});
 
-    const result = confirm(`Are you sure you want to delete "${this.task.title}"?`);
+		const result = confirm(
+			`Are you sure you want to delete "${this.task.title}"?`
+		);
 
-    if (result && this.task.id) {
-      this.store.dispatch(TaskActions.deleteTask({ id: this.task.id }));
-      this.goBack();
-    }
-  }
+		if (result && this.task.id) {
+			this.store.dispatch(TaskActions.deleteTask({ id: this.task.id }));
+			this.goBack();
+		}
+	}
 
-  goToTaskUpdate(id: string): void {
-    this.router.navigate(['/task/update', id]);
-  }
+	goToTaskUpdate(id: string): void {
+		this.router.navigate(['/task/update', id]);
+	}
 
-  deleteAttachment() : void {
-    this.task$!.subscribe(task => {
-      if (!task) {
-        return;
-      }
+	deleteAttachment(): void {
+		this.task$!.subscribe((task) => {
+			if (!task) {
+				return;
+			}
 
-      this.task = task;
-    });
+			this.task = task;
+		});
 
-    const result = confirm(`Are you sure you want to delete "${this.task.attachment}"?`);
+		const result = confirm(
+			`Are you sure you want to delete "${this.task.attachment}"?`
+		);
 
-    if (result && this.task.attachment) {
-      this.store.dispatch(TaskActions.deleteAttachment({ task: this.task }));
-      this.store.dispatch(TaskActions.loadTask({ id: this.task.id }));
-    }
-  }
+		if (result && this.task.attachment) {
+			this.store.dispatch(TaskActions.deleteAttachment({ task: this.task }));
+			this.store.dispatch(TaskActions.loadTask({ id: this.task.id }));
+		}
+	}
 
-  goBack() {
-    this._location.back();
-  }
+	goBack() {
+		this._location.back();
+	}
 
-  onFileSelected(event: Event): void {
-    this.task$!.subscribe(task => {
-      if (!task) {
-        console.error('Task not found');
-        return;
-      }
+	onFileSelected(event: Event): void {
+		this.task$!.subscribe((task) => {
+			if (!task) {
+				console.error('Task not found');
+				return;
+			}
 
-      this.task = task;
-    });
-    
-    const inputElement = event.target as HTMLInputElement;
-    if (inputElement.files && inputElement.files.length > 0) {
-      const file = inputElement.files[0];
-      this.store.dispatch(TaskActions.updateAttachment({ task: this.task, filename: file.name }));
-      this.store.dispatch(TaskActions.loadTask({ id: this.task.id }));
-    }
-  }
+			this.task = task;
+		});
 
-  handleAtSymbol(event: KeyboardEvent): void {
-    const inputText = (event.target as HTMLInputElement).value;
-    const lastIndex = inputText.lastIndexOf('@');
-    if (lastIndex !== -1) {
-      const prevChar = lastIndex > 0 ? inputText[lastIndex - 1] : '';
-      if (prevChar === ' ' || prevChar === '') {
-        const searchText = inputText.substring(lastIndex + 1);
-        this.filteredUsernames = this.allUsernames.filter(username =>
-          username.toLowerCase().includes(searchText.toLowerCase())
-        );
-        this.showMentionBox = this.filteredUsernames.length > 0;
-      } else {
-        this.showMentionBox = false;
-      }
-    } else {
-      this.showMentionBox = false;
-    }
-  }
+		const inputElement = event.target as HTMLInputElement;
+		if (inputElement.files && inputElement.files.length > 0) {
+			const file = inputElement.files[0];
+			this.store.dispatch(
+				TaskActions.updateAttachment({ task: this.task, filename: file.name })
+			);
+			this.store.dispatch(TaskActions.loadTask({ id: this.task.id }));
+		}
+	}
 
-  selectUsername(username: string): void {
-    const inputText = this.newComment;
-    const lastIndex = inputText.lastIndexOf('@');
-    if (lastIndex !== -1) {
-      const prevChar = lastIndex > 0 ? inputText[lastIndex - 1] : '';
-      if (prevChar === ' ' || prevChar === '') {
-        const newText = inputText.substring(0, lastIndex) + '@' + username + ' ';
-        this.newComment = newText;
-        this.showMentionBox = false;
-        setTimeout(() => {
-          this.commentInput.nativeElement.focus();
-        });
-      }
-    }
-  }
+	handleAtSymbol(event: KeyboardEvent): void {
+		const inputText = (event.target as HTMLInputElement).value;
+		const lastIndex = inputText.lastIndexOf('@');
+		if (lastIndex !== -1) {
+			const prevChar = lastIndex > 0 ? inputText[lastIndex - 1] : '';
+			if (prevChar === ' ' || prevChar === '') {
+				const searchText = inputText.substring(lastIndex + 1);
+				this.filteredUsernames = this.allUsernames.filter((username) =>
+					username.toLowerCase().includes(searchText.toLowerCase())
+				);
+				this.showMentionBox = this.filteredUsernames.length > 0;
+			} else {
+				this.showMentionBox = false;
+			}
+		} else {
+			this.showMentionBox = false;
+		}
+	}
 
-  highlightUsername(commentBody: string): string {
-    return commentBody.replace(/@(\w+)/g, '<span class="text-primary">@$1</span>');
-  }
+	selectUsername(username: string): void {
+		const inputText = this.newComment;
+		const lastIndex = inputText.lastIndexOf('@');
+		if (lastIndex !== -1) {
+			const prevChar = lastIndex > 0 ? inputText[lastIndex - 1] : '';
+			if (prevChar === ' ' || prevChar === '') {
+				const newText = inputText.substring(0, lastIndex) + '@' + username + ' ';
+				this.newComment = newText;
+				this.showMentionBox = false;
+				setTimeout(() => {
+					this.commentInput.nativeElement.focus();
+				});
+			}
+		}
+	}
 
-  deleteComment(commentId: string): void {
-    this.task$!.subscribe(task => {
-      if (!task) {
-        console.error('Task not found');
-        return;
-      }
+	highlightUsername(commentBody: string): string {
+		return commentBody.replace(
+			/@(\w+)/g,
+			'<span class="text-primary">@$1</span>'
+		);
+	}
 
-      this.task = task;
+	deleteComment(commentId: string): void {
+		this.task$!.subscribe((task) => {
+			if (!task) {
+				console.error('Task not found');
+				return;
+			}
 
-      const updatedComments = task.comments.filter(comment => comment.id !== commentId);
+			this.task = task;
 
-      const updatedTask: Task = {
-        ...task,
-        comments: updatedComments
-      };
+			const updatedComments = task.comments.filter(
+				(comment) => comment.id !== commentId
+			);
 
-      this.store.dispatch(TaskActions.updateTask({ task: updatedTask }));
-      this.store.dispatch(TaskActions.loadTask({ id: this.task.id }));
-    });
-  }
+			const updatedTask: Task = {
+				...task,
+				comments: updatedComments,
+			};
 
-  postComment(): void {
-    this.task$!.subscribe(task => {
-      if (!task) {
-        console.error('Task not found');
-        return;
-      }
+			this.store.dispatch(TaskActions.updateTask({ task: updatedTask }));
+			this.store.dispatch(TaskActions.loadTask({ id: this.task.id }));
+		});
+	}
 
-      this.task = task;
+	postComment(): void {
+		this.task$!.subscribe((task) => {
+			if (!task) {
+				console.error('Task not found');
+				return;
+			}
 
-      
-    });
+			this.task = task;
+		});
 
-    const updatedTask: Task = {
-      ...this.task,
-      comments: [
-        ...this.task.comments,
-        {
-          id: this.taskService.generateRandomId(),
-          body: this.newComment,
-          username: this.authenticationService.getCurrentUser().username
-        }
-      ]
-    };
+		const updatedTask: Task = {
+			...this.task,
+			comments: [
+				...this.task.comments,
+				{
+					id: this.taskService.generateRandomId(),
+					body: this.newComment,
+					username: this.authenticationService.getCurrentUser().username,
+				},
+			],
+		};
 
-    this.store.dispatch(TaskActions.updateTask({ task: updatedTask }));
-    this.newComment = '';
-    this.store.dispatch(TaskActions.loadTask({ id: this.task.id }));
-  }
+		this.store.dispatch(TaskActions.updateTask({ task: updatedTask }));
+		this.newComment = '';
+		this.store.dispatch(TaskActions.loadTask({ id: this.task.id }));
+	}
 }
